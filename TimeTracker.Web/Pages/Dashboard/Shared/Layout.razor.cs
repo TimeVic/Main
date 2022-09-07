825f759fea251745010225484c4531dc9546c5b4
lampego <lampego@gmail.com>
2022-09-07 08:58:19 +0300

    AB#379 Added empty component

---
+++ TimeTracker.Web/Pages/Dashboard/Shared/Layout.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeTracker.Web.Pages.Dashboard.Shared;
+
+public partial class Layout
+{
+    protected override async Task OnInitializedAsync()
+    {
+        IsRedirectIfNotLoggedIn = true;
+        await base.OnInitializedAsync();
+    }
+}
