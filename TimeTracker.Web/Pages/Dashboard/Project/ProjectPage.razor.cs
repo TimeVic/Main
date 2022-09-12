b0572497b5fe91a53ed695f3de50233f870db37a
lampego <lampego@gmail.com>
2022-09-12 16:37:35 +0300

    AB#405 Added Project page

--- TimeTracker.Web/Pages/Dashboard/Project/ProjectPage.razor.cs
+++ TimeTracker.Web/Pages/Dashboard/Project/ProjectPage.razor.cs
@@ -1,11 +1,33 @@
-﻿using TimeTracker.Web.Store.Project;
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using TimeTracker.Api.Shared.Dto.Entity;
+using TimeTracker.Web.Constants;
+using TimeTracker.Web.Store.Project;
 
 namespace TimeTracker.Web.Pages.Dashboard.Project;
 
 public partial class ProjectPage
 {
-    protected override async Task OnInitializedAsync()
+    [Parameter]
+    public long ProjectId { get; set; }
+
+    [Inject]
+    private IState<ProjectState> _state { get; set; }
+
+    [Inject]
+    private NavigationManager _navigationManager { get; set; }
+    
+    private ProjectDto? _project;
+    
+    protected override async Task OnParametersSetAsync()
     {
-        await base.OnInitializedAsync();
+        await base.OnParametersSetAsync();
+        Dispatcher.Dispatch(new LoadProjectListAction());
+
+        _project = _state.Value.List.FirstOrDefault(item => item.Id == ProjectId);
+        if (_project == null)
+        {
+            _navigationManager.NavigateTo(SiteUrl.Dashboard_Projects);
+        }
     }
 }
