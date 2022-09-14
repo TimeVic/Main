1a7c1bdcb6e12a1c709e3eaf0f4133465eaf8b49
lampego <lampego@gmail.com>
2022-09-14 13:13:47 +0300

    AB#408 Added payment model

---
+++ TimeTracker.Business.Orm/Entities/PaymentEntity.cs
@@ -0,0 +1,50 @@
+using Domain.Abstractions;
+using NHibernate.Mapping.Attributes;
+using NHibernate.Type;
+
+namespace TimeTracker.Business.Orm.Entities
+{
+    [Class(Table = "payments")]
+    public class PaymentEntity: IEntity
+    {
+        [Id(Name = "Id", Generator = "native")]
+        [Column(Name = "id", SqlType = "bigint", NotNull = true)]
+        public virtual long Id { get; set; }
+        
+        [Property(NotNull = true, TypeType = typeof(UtcDateTimeType))]
+        [Column(Name = "payment_time", SqlType = "datetime", NotNull = true)]
+        public virtual DateTime PaymentTime { get; set; }
+        
+        [Property(NotNull = false)]
+        [Column(Name = "description", Length = 512, NotNull = false)]
+        public virtual string? Description { get; set; }
+        
+        [Property(NotNull = true)]
+        [Column(Name = "amount", NotNull = true)]
+        public virtual decimal Amount { get; set; }
+        
+        [Property(NotNull = true, TypeType = typeof(UtcDateTimeType))]
+        [Column(Name = "create_time", SqlType = "datetime", NotNull = true)]
+        public virtual DateTime CreateTime { get; set; }
+        
+        [Property(NotNull = true, TypeType = typeof(UtcDateTimeType))]
+        [Column(Name = "update_time", SqlType = "datetime", NotNull = true)]
+        public virtual DateTime UpdateTime { get; set; }
+        
+        [ManyToOne(
+            ClassType = typeof(ProjectEntity), 
+            Column = "project_id", 
+            Lazy = Laziness.False,
+            Cascade = "none"
+        )]
+        public virtual ProjectEntity? Project { get; set; }
+        
+        [ManyToOne(
+            ClassType = typeof(ClientEntity), 
+            Column = "client_id", 
+            Lazy = Laziness.False,
+            Cascade = "none"
+        )]
+        public virtual ClientEntity Client { get; set; }
+    }
+}
---
+++ TimeTracker.Migrations/Migrations/6_CreateTable_Payments.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using TimeTracker.Migrations.Code;
+
+namespace TimeTracker.Migrations.Migrations
+{
+    [Migration(6)]
+    public class _6_CreateTable_Payments : MyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("payments")
+                .WithColumn("id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("project_id").AsInt64().Nullable()
+                .WithColumn("client_id").AsInt64().NotNullable()
+                .WithColumn("payment_time").AsCustom("timestamptz").NotNullable()
+                .WithColumn("amount").AsDecimal(8, 2).NotNullable()
+                .WithColumn("description").AsString(512).Nullable()
+                .WithColumn("create_time").AsCustom("timestamptz").NotNullable()
+                .WithColumn("update_time").AsCustom("timestamptz").NotNullable();
+            
+            base.Up();
+        }
+    }
+}
