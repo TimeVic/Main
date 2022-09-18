c44ea3dc3f92c24ea78bd9c4cd21e3bf59d1e002
lampego <lampego@gmail.com>
2022-09-18 22:55:57 +0300

    AB#413 Fix time entry creation bug

--- TimeTracker.WorkerServices/Services/TimeEntryStoppingHostedService.cs
+++ TimeTracker.WorkerServices/Services/TimeEntryStoppingHostedService.cs
@@ -1,3 +1,4 @@
+using Persistence.Transactions.Behaviors;
 using TimeTracker.Business.Orm.Constants;
 using TimeTracker.Business.Services.Queue;
 using TimeTracker.Business.Services.TimeEntry;
@@ -8,13 +9,16 @@ namespace TimeTracker.WorkerServices.Services
     internal class TimeEntryStoppingHostedService : ABackgroundService
     {
         private readonly ITimeEntryService _timeEntryService;
-        
+        private readonly IDbSessionProvider _sessionProvider;
+
         public TimeEntryStoppingHostedService(
             ILogger<TimeEntryStoppingHostedService> logger,
-            ITimeEntryService timeEntryService
+            ITimeEntryService timeEntryService,
+            IDbSessionProvider sessionProvider
         ) : base(logger)
         {
             _timeEntryService = timeEntryService;
+            _sessionProvider = sessionProvider;
             ServiceName = "NotificationProcessingHostedService";
         }
 
@@ -23,6 +27,7 @@ namespace TimeTracker.WorkerServices.Services
         protected override async Task DoWorkAsync(CancellationToken cancellationToken)
         {
             await _timeEntryService.StopActiveEntriesFromPastDayAsync(cancellationToken);
+            await _sessionProvider.PerformCommitAsync(cancellationToken);
         }
     }
 }
