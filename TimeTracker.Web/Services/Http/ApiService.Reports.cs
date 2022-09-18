1a337d0f76a66abd301f0ec26f9e913ba9869a8d
lampego <lampego@gmail.com>
2022-09-18 19:33:53 +0300

    AB#420 Added Reports item to menu

---
+++ TimeTracker.Web/Services/Http/ApiService.Reports.cs
@@ -0,0 +1,12 @@
+﻿using TimeTracker.Api.Shared.Constants;
+using TimeTracker.Api.Shared.Dto.Entity;
+using TimeTracker.Api.Shared.Dto.RequestsAndResponses.Dashboard.Payment;
+using TimeTracker.Web.Core.Exceptions;
+
+namespace TimeTracker.Web.Services.Http
+{
+    public partial class ApiService
+    {
+        
+    }
+}
