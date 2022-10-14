c5212df1dfe4a9b58a64ec9625b6d412b76fa9ee
lampego <lampego@gmail.com>
2022-10-14 18:48:20 +0300

    AB#493 Fixed tests

--- TimeTracker.Tests.Integration.Business/Db/Dao/Project/GetListTest.cs
+++ TimeTracker.Tests.Integration.Business/Db/Dao/Project/GetListTest.cs
@@ -51,7 +51,7 @@ public class GetListTest: BaseTest
         await _projectSeeder.CreateSeveralAsync(_workspace, _user, expectedCounter);
 
         var user2 = await _userSeeder.CreateActivatedAsync();
-        await _projectSeeder.CreateSeveralAsync(_workspace, user2, 15);
+        await _projectSeeder.CreateSeveralAsync(user2.DefaultWorkspace, user2, 15);
         
         var actualList = await _projectDao.GetListAsync(_workspace);
         Assert.Equal(expectedCounter, actualList.TotalCount);
