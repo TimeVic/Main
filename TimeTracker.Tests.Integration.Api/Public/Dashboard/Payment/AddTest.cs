0b3cec9508296b47c281e306409d6839a25ff251
lampego <lampego@gmail.com>
2022-10-16 09:11:41 +0300

    AB#491 An user with role user can add a payment

--- TimeTracker.Tests.Integration.Api/Public/Dashboard/Payment/AddTest.cs
+++ TimeTracker.Tests.Integration.Api/Public/Dashboard/Payment/AddTest.cs
@@ -2,12 +2,15 @@ using System.Net;
 using Microsoft.Extensions.DependencyInjection;
 using TimeTracker.Api.Shared.Dto.Entity;
 using TimeTracker.Api.Shared.Dto.RequestsAndResponses.Dashboard.Payment;
+using TimeTracker.Business.Common.Constants;
 using TimeTracker.Business.Common.Exceptions.Api;
 using TimeTracker.Business.Extensions;
 using TimeTracker.Business.Orm.Dao;
 using TimeTracker.Business.Orm.Entities;
 using TimeTracker.Business.Services.Queue;
+using TimeTracker.Business.Testing.Extensions;
 using TimeTracker.Business.Testing.Factories;
+using TimeTracker.Business.Testing.Seeders.Entity;
 using TimeTracker.Tests.Integration.Api.Core;
 
 namespace TimeTracker.Tests.Integration.Api.Public.Dashboard.Payment;
@@ -24,11 +27,13 @@ public class AddTest: BaseTest
     private readonly ClientEntity _client;
     private readonly IProjectDao _projectDao;
     private readonly ProjectEntity _project;
+    private readonly IUserSeeder _userSeeder;
 
     public AddTest(ApiCustomWebApplicationFactory factory) : base(factory)
     {
         _factory = ServiceProvider.GetRequiredService<IDataFactory<PaymentEntity>>();
         _clientDao = ServiceProvider.GetRequiredService<IClientDao>();
+        _userSeeder = ServiceProvider.GetRequiredService<IUserSeeder>();
         _projectDao = ServiceProvider.GetRequiredService<IProjectDao>();
         (_jwtToken, _user) = UserSeeder.CreateAuthorizedAsync().Result;
 
@@ -78,5 +83,72 @@ public class AddTest: BaseTest
         Assert.Equal(payment.Amount, actualPayment.Amount);
         Assert.Equal(payment.Description, actualPayment.Description);
         Assert.Equal(payment.PaymentTime, actualPayment.PaymentTime);
+    }
+    
+    [Fact]
+    public async Task UserWithRoleUserCanAddOwnPayment()
+    {
+        var (otherToken, otherUser) = await _userSeeder.CreateAuthorizedAndShareAsync(
+            _workspace,
+            MembershipAccessType.User
+        );
+        
+        var payment = _factory.Generate();
+        var response = await PostRequestAsync(Url, otherToken, new AddRequest()
+        {
+            WorkspaceId = _workspace.Id,
+            ClientId = _client.Id,
+            Amount = payment.Amount,
+            Description = payment.Description,
+            PaymentTime = payment.PaymentTime
+        });
+        await response.GetJsonDataAsync();
+        response.EnsureSuccessStatusCode();
+
+        var actualPayment = await response.GetJsonDataAsync<PaymentDto>();
+        Assert.True(actualPayment.Id > 0);
+    }
+    
+    [Fact]
+    public async Task UserWithRoleManagerCanAddOwnPayment()
+    {
+        var (otherToken, otherUser) = await _userSeeder.CreateAuthorizedAndShareAsync(
+            _workspace,
+            MembershipAccessType.Manager
+        );
+        
+        var payment = _factory.Generate();
+        var response = await PostRequestAsync(Url, otherToken, new AddRequest()
+        {
+            WorkspaceId = _workspace.Id,
+            ClientId = _client.Id,
+            Amount = payment.Amount,
+            Description = payment.Description,
+            PaymentTime = payment.PaymentTime
+        });
+        await response.GetJsonDataAsync();
+        response.EnsureSuccessStatusCode();
+
+        var actualPayment = await response.GetJsonDataAsync<PaymentDto>();
+        Assert.True(actualPayment.Id > 0);
+    }
+    
+    [Fact]
+    public async Task NonMemberCantAddOwnPayment()
+    {
+        var (otherToken, otherUser) = await _userSeeder.CreateAuthorizedAsync();
+        
+        var payment = _factory.Generate();
+        var response = await PostRequestAsync(Url, otherToken, new AddRequest()
+        {
+            WorkspaceId = _workspace.Id,
+            ClientId = _client.Id,
+            Amount = payment.Amount,
+            Description = payment.Description,
+            PaymentTime = payment.PaymentTime
+        });
+        
+        var responseData = await response.GetJsonErrorAsync();
+        Assert.Equal(new HasNoAccessException().GetTypeName(), responseData.Type);
     }
 }
