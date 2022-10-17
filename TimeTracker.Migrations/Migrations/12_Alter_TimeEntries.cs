e01ee47c792074da363d84535ba7c796fc3e5933
lampego <lampego@gmail.com>
2022-10-17 20:51:36 +0300

    AB#441 Make user_id not nullable

---
+++ TimeTracker.Migrations/Migrations/12_Alter_TimeEntries.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using TimeTracker.Migrations.Code;
+
+namespace TimeTracker.Migrations.Migrations
+{
+    [Migration(12)]
+    public class _12_Alter_TimeEntries : MyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("time_entries")
+                .AlterColumn("user_id").AsInt64().NotNullable();
+            
+            base.Up();
+        }
+    }
+}
