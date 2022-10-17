a347c9eb477ebb4e3da1bc10d0b2769273119345
lampego <lampego@gmail.com>
2022-10-17 20:06:02 +0300

    AB#509 Fixed the bug

---
+++ TimeTracker.Migrations/Migrations/11_Alter_Payments.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+using TimeTracker.Migrations.Code;
+
+namespace TimeTracker.Migrations.Migrations
+{
+    [Migration(11)]
+    public class _11_Alter_Payments : MyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("payments")
+                .AlterColumn("user_id").AsInt64().NotNullable()
+                .AlterColumn("workspace_id").AsInt64().Nullable();
+            
+            base.Up();
+        }
+    }
+}
