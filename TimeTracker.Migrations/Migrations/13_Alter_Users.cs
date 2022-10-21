d188f2f543be20fca284083173ff9b281cb2e8eb
lampego <lampego@gmail.com>
2022-10-21 17:52:43 +0300

    AB#517 Added UTC configuration

---
+++ TimeTracker.Migrations/Migrations/13_Alter_Users.cs
@@ -0,0 +1,55 @@
+﻿using FluentMigrator;
+using TimeTracker.Migrations.Code;
+
+namespace TimeTracker.Migrations.Migrations
+{
+    [Migration(13)]
+    public class _13_Alter_Users : MyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("users")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("workspaces")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("clients")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("projects")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("time_entries")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("queues")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("queues")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("payments")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable()
+                .AlterColumn("payment_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("workspace_setting_clickups")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+            
+            Alter.Table("workspace_memberships")
+                .AlterColumn("create_time").AsDateTime2().NotNullable()
+                .AlterColumn("update_time").AsDateTime2().NotNullable();
+
+            base.Up();
+        }
+    }
+}
--- TimeTracker.Tests.Integration.Business/Db/ConnectionTest.cs
+++ TimeTracker.Tests.Integration.Business/Db/ConnectionTest.cs
@@ -1,12 +1,50 @@
+using Autofac;
+using TimeTracker.Business.Orm.Entities;
+using TimeTracker.Business.Testing.Factories;
 using TimeTracker.Tests.Integration.Business.Core;
 
 namespace TimeTracker.Tests.Integration.Business.Db;
 
 public class ConnectionTest: BaseTest
 {
+    private IDataFactory<UserEntity> _userFactory;
+
+    public ConnectionTest(): base()
+    {
+        _userFactory = Scope.Resolve<IDataFactory<UserEntity>>();
+    }
+
     [Fact]
     public void TestDbConnection()
     {
-        DbSessionProvider.PerformCommitAsync();
+        DbSessionProvider.PerformCommitAsync().Wait();
+    }
+    
+    [Fact]
+    public async Task TimeZoneShouldBeUtc()
+    {
+        var result = await DbSessionProvider.CurrentSession.CreateSQLQuery("show timezone;").ListAsync<string>();
+        Assert.Equal("Etc/UTC", result.First());
+    }
+    
+    [Fact]
+    public async Task ShouldInsertRecordsWithDateTimeInUtcTimezone()
+    {
+        var user = _userFactory.Generate();
+        user.PasswordHash = Array.Empty<byte>();
+        user.PasswordSalt = Array.Empty<byte>();
+        var expectedTime = DateTime.UtcNow;
+        user.CreateTime = expectedTime;
+        user.UpdateTime = expectedTime;
+        
+        var userId = await DbSessionProvider.CurrentSession.SaveAsync(user);
+        await CommitDbChanges();
+
+        user = await DbSessionProvider.CurrentSession.GetAsync<UserEntity>(userId);
+        Assert.Equal(expectedTime.Date, user.CreateTime.Date);
+        Assert.Equal(expectedTime.Hour, user.CreateTime.Hour);
+        Assert.Equal(expectedTime.Minute, user.CreateTime.Minute);
+        Assert.Equal(expectedTime.Second, user.CreateTime.Second);
+        Assert.Equal(expectedTime.Millisecond, user.CreateTime.Millisecond);
     }
 }
