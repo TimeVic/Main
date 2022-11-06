e3712a7893dbdde27be5b8e1cb2f6c7d6e69daa7
lampego <lampego@gmail.com>
2022-11-06 13:29:33 +0200

    AB#549 The owner is workspace membership

--- TimeTracker.Api/Controllers/Dashboard/Workspace/Actions/AddRequestHandler.cs
+++ TimeTracker.Api/Controllers/Dashboard/Workspace/Actions/AddRequestHandler.cs
@@ -5,6 +5,7 @@ using TimeTracker.Api.Shared.Dto.RequestsAndResponses.Dashboard.Workspace;
 using TimeTracker.Business.Common.Constants;
 using TimeTracker.Business.Orm.Dao;
 using TimeTracker.Business.Services.Http;
+using TimeTracker.Business.Services.Security;
 
 namespace TimeTracker.Api.Controllers.Dashboard.Workspace.Actions
 {
@@ -14,18 +15,21 @@ namespace TimeTracker.Api.Controllers.Dashboard.Workspace.Actions
         private readonly IRequestService _requestService;
         private readonly IUserDao _userDao;
         private readonly IWorkspaceDao _workspaceDao;
+        private readonly IWorkspaceAccessService _workspaceAccessService;
 
         public AddRequestHandler(
             IMapper mapper,
             IRequestService requestService,
             IUserDao userDao,
-            IWorkspaceDao workspaceDao
+            IWorkspaceDao workspaceDao,
+            IWorkspaceAccessService workspaceAccessService
         )
         {
             _mapper = mapper;
             _requestService = requestService;
             _userDao = userDao;
             _workspaceDao = workspaceDao;
+            _workspaceAccessService = workspaceAccessService;
         }
     
         public async Task<WorkspaceDto> ExecuteAsync(AddRequest request)
@@ -33,8 +37,9 @@ namespace TimeTracker.Api.Controllers.Dashboard.Workspace.Actions
             var userId = _requestService.GetUserIdFromJwt();
             var user = await _userDao.GetById(userId);
             var workspace = await _workspaceDao.CreateWorkspaceAsync(user, request.Name);
+            await _workspaceAccessService.ShareAccessAsync(workspace, user, MembershipAccessType.Owner);
             var response = _mapper.Map<WorkspaceDto>(workspace);
-            response.CurrentUserAccess = MembershipAccessType.Owner;
+            response.CurrentUserAccess = await _workspaceAccessService.GetAccessTypeAsync(user, workspace);
             return response;
         }
     }
