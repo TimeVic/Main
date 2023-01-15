13150af7deb72cd1cbffc2fb7095df671b7c2fe1
lampego <lampego@gmail.com>
2023-01-15 10:25:36 +0200

    AB#632 Added image cropping helper

---
+++ TimeTracker.Business/Helpers/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace TimeTracker.Business.Helpers;
+
+public static class ImageHelper
+{
+    public static async Task<Image> CropImageFromStreamAsync(string path, int width, int height)
+    {
+        using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        return await CropImageFromStreamAsync(fileStream, width, height);
+    }
+    
+    public static async Task<Image> CropImageFromStreamAsync(Stream imageStream, int width, int height)
+    {
+        var image = await Image.LoadAsync(imageStream);
+        image.Mutate(
+            x => x
+                .AutoOrient()
+                .Resize(new ResizeOptions
+                {
+                    Mode = ResizeMode.Max,
+                    Position = AnchorPositionMode.Center,
+                    Size = new Size(width, height)
+                })
+                .Grayscale()
+        );
+        return image;
+    }
+}
