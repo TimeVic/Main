030a3f0196ef93c29bfd8cea18fa8f454723380a
lampego <lampego@gmail.com>
2023-02-12 00:16:16 +0200

    AB#648 Added base page

---
+++ TimeTracker.Web/Pages/Dashboard/Tasks/TasksPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using TimeTracker.Api.Shared.Dto.Entity;
+using TimeTracker.Web.Store.Project;
+
+namespace TimeTracker.Web.Pages.Dashboard.Tasks;
+
+public partial class TasksPage
+{
+    [Parameter]
+    public long? ClientId { get; set; }
+
+    [Inject]
+    public IState<ProjectState> ProjectState { get; set; }
+
+    public ICollection<ProjectDto> Projects
+    {
+        get
+        {
+            var projects = ProjectState.Value.SortedList;
+            return projects.Where(item => item.Client?.Id == ClientId).ToList();
+        }
+    }
+}
