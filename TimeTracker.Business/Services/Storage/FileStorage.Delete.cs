b6264327d1af1148e2cb6a5449225cff86aae8ce
lampego <lampego@gmail.com>
2023-03-03 14:54:43 +0200

    TMV#377 AWS S3 integration

--- TimeTracker.Business/Services/Storage/FileStorage.Delete.cs
+++ TimeTracker.Business/Services/Storage/FileStorage.Delete.cs
@@ -1,16 +1,5 @@
-﻿using Domain.Abstractions;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Storage.v1.Data;
-using Google.Cloud.Storage.V1;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Persistence.Transactions.Behaviors;
-using TimeTracker.Business.Common.Constants;
+﻿using TimeTracker.Business.Common.Constants;
 using TimeTracker.Business.Common.Exceptions.Api;
-using TimeTracker.Business.Common.Exceptions.Common;
-using TimeTracker.Business.Common.Utils;
-using TimeTracker.Business.Extensions;
-using TimeTracker.Business.Helpers;
 using TimeTracker.Business.Orm.Entities;
 
 namespace TimeTracker.Business.Services.Storage;
@@ -29,7 +18,7 @@ public partial class FileStorage: IFileStorage
             throw new HasNoAccessException();
         }
 
-        await _googleClient.DeleteObjectAsync(_bucketName, file.CloudFilePath);
+        await _s3Client.DeleteObjectAsync(_bucketName, file.CloudFilePath);
         
         file.Tasks.Clear();
         await _dbSessionProvider.CurrentSession.DeleteAsync(file);
--- TimeTracker.Business/Services/Storage/FileStorage.Get.cs
+++ TimeTracker.Business/Services/Storage/FileStorage.Get.cs
@@ -1,16 +1,5 @@
-﻿using Domain.Abstractions;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Storage.v1.Data;
-using Google.Cloud.Storage.V1;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Persistence.Transactions.Behaviors;
-using TimeTracker.Business.Common.Constants;
+﻿using TimeTracker.Business.Common.Constants;
 using TimeTracker.Business.Common.Exceptions.Api;
-using TimeTracker.Business.Common.Exceptions.Common;
-using TimeTracker.Business.Common.Utils;
-using TimeTracker.Business.Extensions;
-using TimeTracker.Business.Helpers;
 using TimeTracker.Business.Orm.Entities;
 
 namespace TimeTracker.Business.Services.Storage;
@@ -29,8 +18,12 @@ public partial class FileStorage: IFileStorage
             throw new HasNoAccessException();
         }
 
-        var fileStream = new MemoryStream();
-        await _googleClient.DownloadObjectAsync(_bucketName, file.CloudFilePath, fileStream);
-        return (file, fileStream);
+        var fileResponse = await _s3Client.GetObjectAsync(_bucketName, file.CloudFilePath);
+        if (fileResponse == null)
+        {
+            throw new RecordNotFoundException($"S3 File not found: {file.CloudFilePath}");
+        }
+
+        return (file, fileResponse.ResponseStream);
     }
 }
