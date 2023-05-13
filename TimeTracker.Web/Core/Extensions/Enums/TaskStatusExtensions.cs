190f7753f724cc505bd76fbe05f3247e56f81e66
lampego <lampego@gmail.com>
2023-05-13 10:58:53 +0300

    TMV#857 Separate tasks by Statuses

---
+++ TimeTracker.Web/Core/Extensions/Enums/TaskStatusExtensions.cs
@@ -0,0 +1,21 @@
+﻿using TaskStatus = TimeTracker.Business.Common.Constants.Task.TaskStatus;
+
+namespace TimeTracker.Web.Core.Extensions.Enums;
+
+public static class TaskStatusExtensions
+{
+    public static string GetColor(this TaskStatus status)
+    {
+        switch (status)
+        {
+            case TaskStatus.Backlog:
+                return "rz-color-base-800";
+            case TaskStatus.Done:
+                return "rz-color-success";
+            case TaskStatus.InProgress:
+                return "rz-color-primary";
+        }
+
+        return "";
+    }
+}
--- TimeTracker.Web/Pages/Dashboard/Tasks/Parts/TasksTable.razor.cs
+++ TimeTracker.Web/Pages/Dashboard/Tasks/Parts/TasksTable.razor.cs
@@ -5,6 +5,7 @@ using Radzen.Blazor;
 using TimeTracker.Api.Shared.Dto.Entity;
 using TimeTracker.Web.Core.Helpers;
 using TimeTracker.Web.Store.Tasks;
+using TaskStatus = TimeTracker.Business.Common.Constants.Task.TaskStatus;
 
 namespace TimeTracker.Web.Pages.Dashboard.Tasks.Parts;
 
@@ -12,6 +13,32 @@ public partial class TasksTable
 {
     [Inject]
     public IState<TasksState> TasksState { get; set; }
+
+    private ICollection<TaskStatus> _statusOrder = new List<TaskStatus>()
+    {
+        TaskStatus.InProgress,
+        TaskStatus.ToDo,
+        TaskStatus.Backlog,
+        TaskStatus.Done
+    };
+
+    private IDictionary<TaskStatus, ICollection<TaskDto>> _taskGroups
+    {
+        get
+        {
+            var tasks = TasksState.Value.List;
+            var result = new Dictionary<TaskStatus, ICollection<TaskDto>>();
+            foreach (var taskStatus in _statusOrder)
+            {
+                result.Add(
+                    taskStatus, 
+                    tasks.Where(item => item.Status == taskStatus).ToList()
+                );
+            }
+
+            return result;
+        }
+    }
 
     private void OnClickTask(TaskDto task)
     {
