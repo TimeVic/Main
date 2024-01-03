8b7441a49a8773969be501e9a6dab79ce6b12970
lampego <lampego@gmail.com>
2024-01-03 06:33:44 +0000

    #1502
    Merges pull request #141

---
+++ TimeTracker.Business.Notifications/Services/FirebaseMessagingService.cs
@@ -0,0 +1,79 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.Extensions.Logging;
+using TimeTracker.Business.Common.Utils;
+
+namespace TimeTracker.Business.Notifications.Services;
+
+public class FirebaseMessagingService: IFirebaseMessagingService
+{
+    private const string CredentialsFilepath = "../../../../.credentials/firebase-credentials.json";
+ 
+    private readonly ILogger<FirebaseMessagingService> _logger;
+    
+    private readonly GoogleCredential _credentials;
+    
+    public FirebaseMessagingService(ILogger<FirebaseMessagingService> logger)
+    {
+        _logger = logger;
+        var filePath = Path.Combine(AssemblyUtils.GetAssemblyPath(), CredentialsFilepath);
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"Google Cloud credentials file not found: {filePath}");
+        }
+
+        using var credentialsStream = new FileStream(
+            Path.Combine(AssemblyUtils.GetAssemblyPath(), CredentialsFilepath),
+            FileMode.Open,
+            FileAccess.Read
+        );
+        _credentials = GoogleCredential.FromStream(credentialsStream);
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = _credentials
+        });
+    }
+
+    public Task SendMessage(string toToken, string title, string body)
+    {
+        var message = new Message()
+        {
+            Token = toToken,
+            Notification = new FirebaseAdmin.Messaging.Notification()
+            {
+                Body = body,
+                Title = title,
+                ImageUrl = "https://timevic.com/img/logo/black/clock-128.png"
+            }
+        };
+
+        return SendMessage(message);
+    }
+
+    public Task SendMessage(string toToken, Dictionary<string, string> data)
+    {
+        var message = new Message()
+        {
+            Token = toToken,
+            Data = data
+        };
+
+        return SendMessage(message);
+    }
+
+    private async Task SendMessage(Message message)
+    {
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        try
+        {
+            var response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            // TODO: Remove Token if failed
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+        }
+    }
+}
---
+++ TimeTracker.Web/Services/Messaging/FcmService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using MudBlazor;
+using TimeTracker.Web.Core.Helpers;
+
+namespace TimeTracker.Web.Services.Messaging;
+
+public class FcmService
+{
+    private readonly IJSRuntime _js;
+    private IJSObjectReference? _jsFcmModule;
+
+    public FcmService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task Test()
+    {
+        // _jsFcmModule = await _js.InvokeAsync<IJSObjectReference>("import", "./js/messaging-init.js");
+        // var result = await _jsFcmModule.InvokeAsync<string>("getGcmToken");
+        // Debug.Log(result);
+    }
+}
