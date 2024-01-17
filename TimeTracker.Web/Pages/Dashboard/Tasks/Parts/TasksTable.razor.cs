7d653ee24f22ae67f05fc55151a55fa5b1f378d7
lampego <lampego@gmail.com>
2024-01-17 06:15:27 +0000

    #1502
    Merges pull request #157

--- TimeTracker.Web/Pages/Dashboard/Tasks/Parts/TasksTable.razor.cs
+++ TimeTracker.Web/Pages/Dashboard/Tasks/Parts/TasksTable.razor.cs
@@ -19,33 +19,5 @@ namespace TimeTracker.Web.Pages.Dashboard.Tasks.Parts;
 
 public partial class TasksTable
 {
-    [Inject]
-    public IState<TasksState> TasksState { get; set; }
     
-    [Inject]
-    public IState<TasksListState> TasksListState { get; set; }
-    
-    [Inject]
-    public ModalDialogProviderService ModalDialogProviderService { get; set; }
-    
-    [Inject]
-    public IActionSubscriber ActionSubscriber { get; set; }
-    
-    private ICollection<TaskStatus> _statuses = new List<TaskStatus>()
-    {
-        TaskStatus.Backlog,
-        TaskStatus.ToDo,
-        TaskStatus.InProgress,
-        TaskStatus.Done
-    };
-    
-    private ICollection<TaskDto> _tasks => TasksState.Value.List;
-
-    private async Task OnAddTask(TaskStatus status)
-    {
-        await ModalDialogProviderService.ShowAddTaskModal(
-            taskListId: TasksListState.Value.SelectedTaskListId,
-            taskStatus: status
-        );
-    }
 }
