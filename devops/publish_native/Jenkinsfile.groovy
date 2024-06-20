9d9f747564112f91ed4c5be405fd26d14aa21b64
lampego <lampego@gmail.com>
2024-06-20 22:01:32 +0300

    DevOps config changed

--- devops/publish_native/Jenkinsfile.groovy
+++ devops/publish_native/Jenkinsfile.groovy
@@ -21,6 +21,8 @@ def webAppContainer = new DockerContainer(
     dockerFile: 'devops/publish_native/web/Dockerfile',
 );
 
+def repositoryUrl = scm.locations[0].remote;
+
 properties([
     parameters([
         // https://plugins.jenkins.io/git-parameter/
@@ -33,8 +35,16 @@ properties([
 node('abedor-mainframe-web') {
 
     stage('Show deployment parameters') {
+        echo "Repository: ${repositoryUrl}"
         echo "Environment: ${params.ENVIRONMENT}"
         echo "Tag: ${params.GIT_TAG}"
+    }
+
+    if (!params.GIT_TAG?.trim())
+    {
+        stage('Switch to GIT tag') {
+            git branch: "${params.BRANCH}", url: repositoryUrl
+        }    
     }
 
     stage('CleanUp Docker') {
@@ -65,52 +75,102 @@ node('abedor-mainframe-web') {
     }
 
     stage('Set environment vars') {
-        envVariables.put('App__FrontendUrl', 'https://timevic.com')
-        envVariables.put('Serilog__IsSendEmailIfError', 'false')
-        envVariables.put('Serilog__MinimumLevel__Default', 'Debug')
+        if (params.ENVIRONMENT == 'Production')
+        {
+            envVariables.put('App__FrontendUrl', 'https://timevic.com')
+            envVariables.put('Serilog__IsSendEmailIfError', 'false')
+            envVariables.put('Serilog__MinimumLevel__Default', 'Debug')
 
-        // Redis
-        envVariables.put('Redis__Server', '10.10.0.2:6379')
-    
-        withCredentials([
-                usernamePassword(credentialsId: "timevic_production_db_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
-        ]) {
-            envVariables.put(
-                'ConnectionStrings__DefaultConnection',
-                "User ID=${USER_NAME};Password=${PASSWORD};Host=192.168.99.8;Port=5433;Database=timevic;Pooling=true;"
-            )
-        }
-        withCredentials([
-                usernamePassword(credentialsId: "timevic_production_smtp_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
-        ]) {
-            envVariables.put('Smtp__UserName', USER_NAME)
-            envVariables.put('Smtp__Password', PASSWORD)
-        }
-        withCredentials([string(credentialsId: "timevic_production_user_jwt", variable: 'AUTH_SECRET')]) {
-            envVariables.put('App__Auth__SymmetricSecurityKey', AUTH_SECRET)
-        }
-        withCredentials([string(credentialsId: "timevic_production_recaptcha_secret", variable: 'AUTH_SECRET')]) {
-            envVariables.put('ReCaptcha__Secret', AUTH_SECRET)
-        }
-        withCredentials([string(credentialsId: "timevic_production_google__storage_project_id", variable: 'AUTH_SECRET')]) {
-            envVariables.put('Google__Storage__ProjectId', AUTH_SECRET)
-        }
+            // Redis
+            envVariables.put('Redis__Server', '10.10.0.2:6379')
+        
+            withCredentials([
+                    usernamePassword(credentialsId: "timevic_production_db_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put(
+                    'ConnectionStrings__DefaultConnection',
+                    "User ID=${USER_NAME};Password=${PASSWORD};Host=192.168.99.8;Port=5433;Database=timevic;Pooling=true;"
+                )
+            }
+            withCredentials([
+                    usernamePassword(credentialsId: "timevic_production_smtp_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put('Smtp__UserName', USER_NAME)
+                envVariables.put('Smtp__Password', PASSWORD)
+            }
+            withCredentials([string(credentialsId: "timevic_production_user_jwt", variable: 'AUTH_SECRET')]) {
+                envVariables.put('App__Auth__SymmetricSecurityKey', AUTH_SECRET)
+            }
+            withCredentials([string(credentialsId: "timevic_production_recaptcha_secret", variable: 'AUTH_SECRET')]) {
+                envVariables.put('ReCaptcha__Secret', AUTH_SECRET)
+            }
+            withCredentials([string(credentialsId: "timevic_production_google__storage_project_id", variable: 'AUTH_SECRET')]) {
+                envVariables.put('Google__Storage__ProjectId', AUTH_SECRET)
+            }
 
-        withCredentials([string(credentialsId: "timevic_production_google__storage_bucket_name", variable: 'AUTH_SECRET')]) {
-            envVariables.put('Google__Storage__BucketName', AUTH_SECRET)
+            withCredentials([string(credentialsId: "timevic_production_google__storage_bucket_name", variable: 'AUTH_SECRET')]) {
+                envVariables.put('Google__Storage__BucketName', AUTH_SECRET)
+            }
+            
+            withCredentials([
+                usernamePassword(credentialsId: "timevic_production_aws_s3_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put('AWS__S3__AccessKey', USER_NAME)
+                envVariables.put('AWS__S3__SecretKey', PASSWORD)
+            }
+            
+            withCredentials([string(credentialsId: "timevic_production_aws_s3_bucket_name", variable: 'AUTH_SECRET')]) {
+                envVariables.put('AWS__S3__BucketName', AUTH_SECRET)
+            }
         }
+        else if (params.ENVIRONMENT == 'Development')
+        {
+            envVariables.put('App__FrontendUrl', 'https://dev.timevic.com')
+            envVariables.put('Serilog__IsSendEmailIfError', 'false')
+            envVariables.put('Serilog__MinimumLevel__Default', 'Debug')
+
+            // Redis
+            envVariables.put('Redis__Server', '10.10.0.2:6379')
         
-        withCredentials([
-            usernamePassword(credentialsId: "timevic_production_aws_s3_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
-        ]) {
-            envVariables.put('AWS__S3__AccessKey', USER_NAME)
-            envVariables.put('AWS__S3__SecretKey', PASSWORD)
+            withCredentials([
+                    usernamePassword(credentialsId: "timevic_production_db_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put(
+                    'ConnectionStrings__DefaultConnection',
+                    "User ID=${USER_NAME};Password=${PASSWORD};Host=192.168.99.8;Port=5433;Database=timevic;Pooling=true;"
+                )
+            }
+            withCredentials([
+                    usernamePassword(credentialsId: "timevic_production_smtp_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put('Smtp__UserName', USER_NAME)
+                envVariables.put('Smtp__Password', PASSWORD)
+            }
+            withCredentials([string(credentialsId: "timevic_production_user_jwt", variable: 'AUTH_SECRET')]) {
+                envVariables.put('App__Auth__SymmetricSecurityKey', AUTH_SECRET)
+            }
+            withCredentials([string(credentialsId: "timevic_production_recaptcha_secret", variable: 'AUTH_SECRET')]) {
+                envVariables.put('ReCaptcha__Secret', AUTH_SECRET)
+            }
+            withCredentials([string(credentialsId: "timevic_production_google__storage_project_id", variable: 'AUTH_SECRET')]) {
+                envVariables.put('Google__Storage__ProjectId', AUTH_SECRET)
+            }
+
+            withCredentials([string(credentialsId: "timevic_production_google__storage_bucket_name", variable: 'AUTH_SECRET')]) {
+                envVariables.put('Google__Storage__BucketName', AUTH_SECRET)
+            }
+            
+            withCredentials([
+                usernamePassword(credentialsId: "timevic_production_aws_s3_credentials", usernameVariable: 'USER_NAME', passwordVariable: 'PASSWORD')
+            ]) {
+                envVariables.put('AWS__S3__AccessKey', USER_NAME)
+                envVariables.put('AWS__S3__SecretKey', PASSWORD)
+            }
+            
+            withCredentials([string(credentialsId: "timevic_production_aws_s3_bucket_name", variable: 'AUTH_SECRET')]) {
+                envVariables.put('AWS__S3__BucketName', AUTH_SECRET)
+            }
         }
-        
-        withCredentials([string(credentialsId: "timevic_production_aws_s3_bucket_name", variable: 'AUTH_SECRET')]) {
-            envVariables.put('AWS__S3__BucketName', AUTH_SECRET)
-        }
-        
     }
 
 //     stage('Stop containers') {
