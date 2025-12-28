bf17c27c7fea96fbb83563d59340585b2661d28e
lampego <lampego@gmail.com>
2025-12-28 18:35:12 +0200

    PR-3237 Changed time picker

--- TimeTracker.Business.Common/Services/Format/ITimeParsingService.cs
+++ TimeTracker.Business.Common/Services/Format/ITimeParsingService.cs
@@ -6,9 +6,13 @@ public interface ITimeParsingService: IDomainService
 {
     string FormatTime(string timeString);
 
-    TimeSpan ParseTime(string timeString);
+    TimeSpan ParseTimeSpan(string timeString);
 
     TimeSpan GetTimeSpanFromDateTime(DateTime dateTime);
 
     string TimeSpanToTimeString(TimeSpan timeSpan, bool isAddSecond = false);
+
+    string TimeOnlyToStringString(TimeOnly time);
+
+    TimeOnly ParseTimeOnly(string timeString);
 }
