849d1075c9b6b2c2e558a48dfe88385f8d28c4d6
lampego <lampego@gmail.com>
2026-03-21 12:25:51 +0200

    PR-3416 Fixed build

---
+++ TimeTracker.Web/Ui/Pages/Dashboard/Shared/LayoutParts/MainHeader.razor.cs
@@ -0,0 +1,30 @@
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using TimeTracker.Api.Shared.Dto.Entity;
+using TimeTracker.Web.Services.Workspace;
+using TimeTracker.Web.Store.Auth;
+using TimeTracker.Web.Store.Workspace;
+
+namespace TimeTracker.Web.Ui.Pages.Dashboard.Shared.LayoutParts;
+
+public partial class MainHeader
+{
+    [Inject]
+    public IState<AuthState> AuthState { get; set; }
+    
+    [Inject]
+    public IState<WorkspaceState> WorkspaceState { get; set; }
+    
+    [Inject]
+    public WorkspaceInitializationService _workspaceInitialization { get; set; }
+    
+    private void OnSelectWorkspace(WorkspaceDto? workspace)
+    {
+        if (workspace == null)
+        {
+            // Clicked on selected item
+            return;
+        }
+        _workspaceInitialization.ChangeWorkspace(workspace);
+    }
+}
