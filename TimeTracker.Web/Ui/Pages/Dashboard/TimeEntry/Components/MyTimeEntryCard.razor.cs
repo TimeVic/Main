bdc56f350675a4f0a34a1a7c13a0860a4da77717
lampego <lampego@gmail.com>
2026-03-23 21:22:55 +0200

    PR-3416 Lumex UI implementation. Added Confirmation modal

--- TimeTracker.Web/Ui/Pages/Dashboard/TimeEntry/Components/MyTimeEntryCard.razor.cs
+++ TimeTracker.Web/Ui/Pages/Dashboard/TimeEntry/Components/MyTimeEntryCard.razor.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using TimeTracker.Api.Shared.Dto.Entity;
 using TimeTracker.Business.Common.Services.Format;
+using TimeTracker.Web.Store.TimeEntry;
 
 namespace TimeTracker.Web.Ui.Pages.Dashboard.TimeEntry.Components;
 
@@ -14,17 +15,20 @@ public partial class MyTimeEntryCard
     
     [Parameter]
     public EventCallback<TimeEntryDto> OnDelete { get; set; }
-
+    
+    [Parameter]
+    public EventCallback<TimeEntryDto> OnClone { get; set; }
+    
     [Inject]
     private ITimeParsingService _timeParsingService { get; set; }
     
     private string GetProjectLabel()
     {
-        return string.IsNullOrWhiteSpace(Entry.Project?.Name) ? "No project" : Entry.Project?.Name ?? string.Empty;
+        return string.IsNullOrWhiteSpace(Entry.Project?.Name) ? "No project" : Entry.Project?.Name ?? "No project";
     }
 
     private string GetTaskLabel()
     {
-        return Entry.Task?.TaskId != null ? "No task" : Entry.Task?.TaskId.ToString() ?? string.Empty;
+        return Entry.Task?.TaskId != null ? "No task" : Entry.Task?.TaskId.ToString() ?? "No task";
     }
 }
