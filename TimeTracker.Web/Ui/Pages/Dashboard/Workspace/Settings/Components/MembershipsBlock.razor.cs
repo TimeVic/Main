4e218e02bc26baf456324df6769e6fa18772fcc4
lampego <lampego@gmail.com>
2026-04-18 19:56:09 +0300

    PR-10143 Aded manage mambers tab

---
+++ TimeTracker.Web/Ui/Pages/Dashboard/Workspace/Settings/Components/MembershipsBlock.razor.cs
@@ -0,0 +1,25 @@
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using TimeTracker.Api.Shared.Dto.Entity;
+using TimeTracker.Web.Store.WorkspaceMemberships;
+
+namespace TimeTracker.Web.Ui.Pages.Dashboard.Workspace.Settings.Components;
+
+public partial class MembershipsBlock
+{
+    [Inject]
+    private IState<WorkspaceMembershipsState> _state { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Dispatcher.Dispatch(new LoadListAction());
+    }
+
+    private Task OnDelete(WorkspaceMembershipDto membership)
+    {
+        Dispatcher.Dispatch(new DeleteMemberAction(membership));
+        return Task.CompletedTask;
+    }
+}
+
---
+++ TimeTracker.Web/Ui/Pages/Dashboard/Workspace/Settings/Components/Parts/AddMemberForm.razor.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Forms;
+using TimeTracker.Web.Store.WorkspaceMemberships;
+
+namespace TimeTracker.Web.Ui.Pages.Dashboard.Workspace.Settings.Components.Parts;
+
+public partial class AddMemberForm
+{
+    private InviteModel _model = new();
+    private EditForm _form = default!;
+
+    private Task Submit()
+    {
+        if (!_form.EditContext!.Validate())
+        {
+            return Task.CompletedTask;
+        }
+
+        Dispatcher.Dispatch(new AddNewMemberAction(_model.Email));
+        _model = new InviteModel();
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private class InviteModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
+
