6a13a03488364fdb1da25b086ca873388706c765
lampego <lampego@gmail.com>
2026-05-17 15:54:42 +0300

    PR-10331 Changed task list management flow

--- TimeTracker.Web/Ui/Pages/Dashboard/Tasks/Components/TasksBlock.razor.cs
+++ TimeTracker.Web/Ui/Pages/Dashboard/Tasks/Components/TasksBlock.razor.cs
@@ -4,7 +4,6 @@ using Fluxor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using TimeTracker.Api.Shared.Dto.Entity.Task;
-using TimeTracker.Web.Store.Project;
 using TimeTracker.Web.Store.Tasks;
 using TimeTracker.Web.Store.TasksList;
 using TaskStatus = TimeTracker.Business.Common.Constants.Task.TaskStatus;
@@ -17,9 +16,6 @@ public partial class TasksBlock: IDisposable
     public IActionSubscriber ActionSubscriber { get; set; }
     
     [Inject]
-    public IState<ProjectState> _projectState { get; set; }
-    
-    [Inject]
     public IState<TasksListState> _tasksListState { get; set; }
     
     [Inject]
@@ -28,29 +24,10 @@ public partial class TasksBlock: IDisposable
     private TaskListDto? _taskList => _tasksListState.Value.SelectedTaskList;
     private readonly Subject<ICollection<TaskDto>> _tasksSubject = new();
     private bool _isShowAddTaskModal = false;
-    private bool _isShowUpdateTaskListModal = false;
-    private bool _isShowDeleteTaskListConfirmation = false;
     
     public void Dispose()
     {
         ActionSubscriber.UnsubscribeFromAllActions(this);
         _tasksSubject.Dispose();
-    }
-
-    private Task OnEditTaskList()
-    {
-        if (_taskList == null)
-        {
-            return Task.CompletedTask;
-        }
-
-        _isShowUpdateTaskListModal = true;
-        return Task.CompletedTask;
-    }
-    
-    private Task OnDeleteTaskList()
-    {
-        Dispatcher.Dispatch(new ArchiveTaskListAction(_taskList!));
-        return Task.CompletedTask;
     }
 }
