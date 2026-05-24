a331571866530018294d58da0334733586ebb69e
lampego <lampego@gmail.com>
2026-05-24 13:21:19 +0300

    PR-10408 Added Mobile App project

---
+++ TimeTracker.Mobile/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeTracker.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "TimeTracker.Mobile" };
+    }
+}
---
+++ TimeTracker.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeTracker.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
