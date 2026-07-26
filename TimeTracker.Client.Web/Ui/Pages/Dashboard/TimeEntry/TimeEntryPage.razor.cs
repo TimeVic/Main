73fc71ea23a355b92efec664faab3fc521eb70b8
lampego <lampego@gmail.com>
2026-07-26 07:34:15 +0300

    PR-10785 Refactor time entry state management by removing visibility state and updating reload logic for time entries page

--- TimeTracker.Client.Web/Ui/Pages/Dashboard/TimeEntry/TimeEntryPage.razor.cs
+++ TimeTracker.Client.Web/Ui/Pages/Dashboard/TimeEntry/TimeEntryPage.razor.cs
@@ -2,18 +2,12 @@
 
 namespace TimeTracker.Client.Web.Ui.Pages.Dashboard.TimeEntry;
 
-public partial class TimeEntryPage: BaseReactiveComponent, IDisposable
+public partial class TimeEntryPage: BaseReactiveComponent
 {
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
-        Dispatcher.Dispatch(new TimeTracker.Client.Core.Store.TimeEntry.SetIsTimeEntryListVisibleAction(true));
         Dispatcher.Dispatch(new TimeTracker.Client.Core.Store.TimeEntry.SetSelectedPageAction(1));
         Dispatcher.Dispatch(new TimeTracker.Client.Core.Store.TimeEntry.LoadListAction());
-    }
-
-    public void Dispose()
-    {
-        Dispatcher.Dispatch(new TimeTracker.Client.Core.Store.TimeEntry.SetIsTimeEntryListVisibleAction(false));
     }
 }
